2e643570fdcb378440082f2c0f47c79f16ad447d
namnahyeon <skaskgus0704@naver.com>
2019-06-08 14:54:20 +0900

    이미지 추가

--- src/pages/heroes/Heroes.js
+++ src/pages/heroes/Heroes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios/index';
-import './Heroes.scss';
+import styles from './Heroes.moudle.scss';
 
 export class Heroes extends React.Component {
   constructor(props) {
@@ -24,11 +24,15 @@ export class Heroes extends React.Component {
 
   render() {
     return (
-      <ul className="img-box">
+      <ul className={styles.imgBox}>
         {this.state.heroes.map(hero => (
           <li key={hero.hero_id} className="row align-items-center m-0">
             <div className="col-1 py-2">
-              <img src={hero.photo} alt={hero.name} className="img-fluid rounded-circle" />
+              <img src={hero.photo ? hero.photo : process.env.PUBLIC_URL + '/images/baseline-face-24px.svg'}
+                   alt={hero.name}
+                   className="img-fluid rounded-circle" style={{width: '100%'}}
+                   style={{width : '100%'}}
+              />
             </div>
             <span className="col">{hero.name}</span>
           </li>
