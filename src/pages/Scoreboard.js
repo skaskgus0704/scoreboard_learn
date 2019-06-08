52a69b2ebbe5398034f765bb2b84d9ca0b110ad7
최철환 <hwanyhee@naver.com>
2019-06-08 11:06:43 +0900

    scoreboard 리팩토링 1

--- src/pages/Scoreboard.js
+++ src/pages/Scoreboard.js
@@ -1,15 +1,68 @@
 import React from 'react';
 
-export class Scoreboard extends React.Component {
-  constructor(props) {
-    super(props);
+import Header from "../components/Header";
+import {Player} from "../components/Player";
+import AddPlayerForm from "../components/AddPlayerForm";
+import {connect} from "react-redux";
+
+class Scoreboard extends React.Component {
+
+  maxId = 4;
+
+  // 1) player 삭제 콜백 펑션 정의
+  handleRemovePlayer = (id) => {
+    console.log(id);
+
+    this.setState(prevState => ({
+      players: prevState.players.filter(item => item.id !== id)
+    }))
   }
+
+  handleChangeScore = (id, delta) => {
+    console.log(id, delta);
+
+
+    this.setState(prevState => {
+      this.state.players.forEach(item => {
+        if (item.id === id) {
+          item.score += delta;
+        }
+      });
+      return {
+        players: [...prevState.players]
+      }
+    })
+  }
+
+  /*  handleAddPlayer = (name) => {
+      console.log(name);
+      this.setState(prevState => ({
+        players: [...prevState.players, {name, score: 0, id: ++this.maxId}]
+      }))
+    }*/
+  // shorthand property: key와 value가 같을 경우 하나만 사용
 
   render() {
     return (
-      <div>
-        Scoreboard works.
+      <div className='scoreboard'>
+        <Header players={this.props.players} />
+
+        {
+          this.props.players.map(player => (
+            <Player name={player.name} key={player.id} id={player.id}
+                    score={player.score}
+                    removePlayer={this.handleRemovePlayer}
+                    changeScore={this.handleChangeScore} />
+          ))
+        }
+        <AddPlayerForm addPlayer={this.handleAddPlayer}/>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  players: state.playerReducer.players
+})
+
+export default connect(mapStateToProps)(Scoreboard);
